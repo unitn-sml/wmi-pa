84e9535e9feba5171d7a2a7ef3cc8af6cf97cbb1
paolomorettin <morettin.paolo@gmail.com>
2019-04-29 23:54:52 +0200

    __init__ checks for required software (mathsat, latte)

--- wmipa/__init__.py
+++ wmipa/__init__.py
@@ -1,3 +1,24 @@
-from .logger import logger
-from .weights import Weights
-from .wmi import WMI
+
+from shutil import which
+
+IMPORT_ERR_MSG = None
+
+# check whether MathSAT is installed or not
+try:
+    import mathsat
+except ImportError:
+    IMPORT_ERR_MSG = """Couldn't import mathsat.
+MathSAT needs to be manually installed via:
+    pysmt-install --msat"""
+
+# check whether LattE integrale is installed or not
+if IMPORT_ERR_MSG is None and which('integrate') is None:
+    IMPORT_ERR_MSG = """LattE integrale is not installed or its binaries are not included in PATH"""
+
+if IMPORT_ERR_MSG:
+    raise ImportError(IMPORT_ERR_MSG)
+else:
+    from .logger import logger
+    from .weights import Weights
+    from .wmi import WMI    
+
