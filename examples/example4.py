b2999a19fba8c8ca7e712ac24decc88346a88c75
paolomorettin <morettin.paolo@gmail.com>
2020-01-16 13:52:49 +0100

    fixed example

--- examples/example4.py
+++ examples/example4.py
@@ -20,7 +20,8 @@ phis = [Bool(True), Iff(a, Bool(True)), LE(x, Real(3))]
 # weight function definition
 w = Ite(a, x, Times(Real(2), x))
 
-chi = And(GE(x, Real(-5)), LE(x, Real(5)),
+chi = And(Or(a, b, c),
+          GE(x, Real(-5)), LE(x, Real(5)),
           Implies(a, GE(x, Real(0))),
           Implies(b, GE(x, Real(2))),
           Implies(c, LE(x, Real(3))))
