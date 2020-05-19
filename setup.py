45cd917e2ab7e3d10953212d24213c56dd386c58
paolomorettin <morettin.paolo@gmail.com>
2020-05-19 11:33:32 +0200

    new version 0.1.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ URL = 'http://github.com/unitn-sml/wmi-pa'
 EMAIL = 'paolo.morettin@unitn.it'
 AUTHOR = 'Paolo Morettin'
 REQUIRES_PYTHON = '>=3.5.0'
-VERSION = "0.1.3"
+VERSION = "0.1.4"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
