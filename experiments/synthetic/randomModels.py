017f0e23b546c955054cc1e8df15922fb8ca8693
masinag <gabriele.masina@virgilio.it>
2022-02-13 00:33:04 +0100

    Fix synthetic timeout

--- experiments/synthetic/randomModels.py
+++ experiments/synthetic/randomModels.py
@@ -223,14 +223,14 @@ def main():
     n_models = args.models
     seedn = args.seed
 
+    if seedn is None:
+        seedn = int(time.time())
+
     output_dir = 'models_r{}_b{}_d{}_m{}_s{}'.format(
         n_reals, n_bools, depth, n_models, seedn)
     output_dir = path.join(output, output_dir)
 
     check_input_output(output, output_dir)
-
-    if seedn is None:
-        seedn = int(time.time())
 
     # init generator
     templ_bools = ModelGenerator.TEMPL_BOOLS
