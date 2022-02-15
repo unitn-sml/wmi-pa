6af05403eceec0b5be2f9ad135ce0df4173e66b7
masinag <gabriele.masina@virgilio.it>
2022-02-15 22:24:41 +0100

    Add scripts to generate and evaluate synthetic experiments

---
+++ experiments/synthetic/synthetic_eval.sh
@@ -0,0 +1,12 @@
+SYN_DIR=synthetic_exp
+
+for dir in $(ls $SYN_DIR/data)
+do
+	mkdir $SYN_DIR/results/$dir
+	echo Evaluating $dir
+	for mode in PAEUFTA PA
+	do
+		echo Mode $mode
+		python3 evaluateModels.py $SYN_DIR/data/$dir -o $SYN_DIR/results/$dir -m $mode 
+	done
+done
