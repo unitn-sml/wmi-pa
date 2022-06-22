5234ea1943e73dfbe333ccdbeb55839911b52a84
Gabriele <gabriele.masina@virgilio.it>
2022-06-22 07:12:36 +0000

    Update DETs generation script

--- experiments/mlc_gen.sh
+++ experiments/mlc_gen.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+N_MIN=100
+N_MAX=200
+N_QUERIES=5
+SEED=666
 
 if [[ ! -d hybrid-benchmarks ]]; then
-  echo "Cloning git@github.com:paolomorettin/hybrid-benchmarks.git..."
-  git clone git@github.com:paolomorettin/hybrid-benchmarks.git
+  echo "Cloning https://github.com/paolomorettin/hybrid-benchmarks.git..."
+  git clone https://github.com/paolomorettin/hybrid-benchmarks.git
 fi
 
 cd hybrid-benchmarks/uai-22
 for qh in 0.0 0.25 0.5 0.75 1.0
 do
-  python3 generate_dets.py 100 200 5 $qh 666
+  python3 generate_dets.py $N_MIN $N_MAX $N_QUERIES $qh $SEED
 done
 
 cd ../../
