51bb86f8d514045dcf6528c68368fa2e378ace57
Gabriele Masina <gabriele.masina@virgilio.it>
2023-08-25 16:23:31 +0200

    Run synthetic experiments with volesti with many seeds and different errors

--- experiments/synthetic_eval.sh
+++ experiments/synthetic_eval.sh
@@ -7,9 +7,17 @@ do
 	res_dir=$(sed "s+data+results+g" <<< $dir)
 	mkdir -p $res_dir
 	echo Evaluating $dir
-	for mode in XSDD XADD FXSDD "PA latte" "SAPA latte" "SAPASK latte" "SAPASK volesti" "SAPASK symbolic"
+	for mode in XSDD XADD FXSDD "PA latte" "SAPA latte" "SAPASK latte" "SAPASK symbolic"
 	do
 		echo Mode $mode
 		python3 evaluateModels.py $dir -o $res_dir -m $mode 
 	done
+	echo "Mode SAPASK volesti, error 0.005, 10 seeds"
+	python3 evaluateModels.py $dir -o $res_dir -m SAPASK volesti -e 0.005 --seed 666 --n-seeds 10
+	echo "Mode SAPASK volesti, error 0.01, 10 seeds"
+	python3 evaluateModels.py $dir -o $res_dir -m SAPASK volesti -e 0.01  --seed 666 --n-seeds 10
+	echo "Mode SAPASK volesti, error 0.05, 10 seeds"
+	python3 evaluateModels.py $dir -o $res_dir -m SAPASK volesti -e 0.05  --seed 666 --n-seeds 10
+	echo "Mode SAPASK volesti, error 0.1, 10 seeds"
+	python3 evaluateModels.py $dir -o $res_dir -m SAPASK volesti -e 0.1   --seed 666 --n-seeds 10
 done
