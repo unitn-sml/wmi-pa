275f899ae004ebd4eb8ba27bbc424cdd599e3927
masinag <gabriele.masina@virgilio.it>
2023-09-19 11:18:02 +0200

    Minor in approx experiments

--- experiments/synthetic_approx_eval.sh
+++ experiments/synthetic_approx_eval.sh
@@ -14,9 +14,9 @@ for dir in $(ls -d $DATA_DIR/*); do
   echo Mode "SAPASK latte";
   python3 evaluateModels.py $dir -o $res_dir -m SAPASK latte
 
-  for N in 10000 100000 1000000; do
+  for N in 1000 10000 100000 1000000; do
     echo "Mode SAPASK volesti, error $ERROR, N $N, 10 seeds"
-    python3 evaluateModels.py $dir -o $res_dir -m SAPASK volesti -e ERROR -N $N --seed $SEED --n-seeds $N_VOLESTI_INSTANCES
+    python3 evaluateModels.py $dir -o $res_dir -m SAPASK volesti -e $ERROR -N $N --seed $SEED --n-seeds $N_VOLESTI_INSTANCES
   done
 
 done
