db04364dde7c911dc0bd7ab907b1fe3d24264abe
paolomorettin <morettin.paolo@gmail.com>
2025-07-03 14:57:05 +0200

    minors

--- benchmarks/big_random/run-old.sh
+++ benchmarks/big_random/run-old.sh
@@ -2,25 +2,11 @@
 #!/bin/bash
 
 TIMEOUT=600 # seconds
-#NINSTANCES=10
-
-#MINREALS=3
-#MAXREALS=8
 
 INPUT_DIR="./input/"
 OUTPUT_DIR="./output/"
 
-
-mkdir $INPUT_DIR
-#for NREALS in $(seq $MINREALS $MAXREALS)
-#do
-#    for ID in $(seq 1 $NINSTANCES)
-#    do
-#	python3 ../synthetic.py $ID --directory $INPUT_DIR --n_reals $NREALS
-#    done
-#done
 mkdir $OUTPUT_DIR
-
 OLDPA_DIR=$OUTPUT_DIR"oldpa/"
 mkdir $OLDPA_DIR
 for FILENAME in $(ls $INPUT_DIR)
