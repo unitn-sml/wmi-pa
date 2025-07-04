206ff60d672e990e8a488924e459852feb94a673
paolomorettin <morettin.paolo@gmail.com>
2025-07-04 09:28:36 +0200

    axis-aligned wrapper

---
+++ wmipa/integration/axisaligned.py
@@ -0,0 +1,75 @@
+
+
+class AxisAlignedWrapper:
+
+    '''
+    Before calling a general self.integrator, checks if:
+    - the polytope is axis-aligned
+    - the integrand is constant
+
+    possibly computing the integral in linear time.
+
+    '''
+
+    def __init__(self, integrator):
+        self.integrator = integrator
+
+    def integrate(self, polytope, polynomial):
+        w = AxisAlignedWrapper._constant_integrand(polynomial)
+        if w is not None:
+            vol = AxisAlignedWrapper._axis_aligned_volume(polytope)
+            if vol is not None:
+                return w * vol
+
+        return self.integrator.integrate(polytope, polynomial)
+
+    def integrate_batch(self, convex_integrals):
+        volumes = []
+        for polytope, polynomial in convex_integrals:
+            volumes.append(self.integrate(polytope, polynomial))
+
+        return np.array(volumes)
+
+    @staticmethod
+    def _constant_integrand(polynomial):
+        if polynomial.degree == 0:
+            return polynomial.monomials.values()[0]
+        else:
+            return None
+
+
+    @staticmethod
+    def _axis_aligned_volume(polytope):
+
+        def parse_bound(inequality):
+            monos = inequality.polynomial.monomials
+            if len(monos) == 1:
+                exp, coeff = monos.items()[0]
+                bound = [0, np.inf] if coeff > 0 else [-np.inf, 0]
+                return exp.index(1), bound
+            elif len(monos) == 2:
+                ckey, exp = inequality.polynomial.ordered_keys
+                const = monos[ckey]
+                coeff = monos[exp]
+                bound = [-const/coeff, np.inf] if coeff > 0 else [-np.inf, const/coeff]
+                return exp.index(1), bound
+            else:
+                return None
+        
+        bounds = [[-np.inf, np.inf] for _ in range(polytope.N)]
+
+        for ineq in polytope.inequalities:
+            nvb = parse_bound(ineq)
+            if nvb is None:
+                return None
+            nvar, new_bound = nvb
+            old_bound = bounds[nvar]
+            bounds[nvar] = [np.max(old_bound[0], new_bound[0]),
+                            np.min(old_bound[1], new_bound[1])]
+
+        barray = np.array(bounds)
+        volume = np.sum(np.abs(np.subtract(barray[:,0], barray[:,1])))
+        if barray < np.inf:
+            return barray
+        else:
+            return None
