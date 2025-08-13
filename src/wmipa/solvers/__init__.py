bc8dc7c3132abeb3fa8801e4d7369aae854178ad
paolomorettin <morettin.paolo@gmail.com>
2025-08-13 10:28:52 +0200

    removed mathsat check from solvers/__init__.py

--- src/wmipa/solvers/__init__.py
+++ src/wmipa/solvers/__init__.py
@@ -1,10 +1,1 @@
-# check whether MathSAT is installed or not
-try:
-    import mathsat
-except ImportError:
-    IMPORT_ERR_MSG = """Couldn't import mathsat.
-MathSAT needs to be manually installed via:
-    pysmt-install --msat"""
-    raise ImportError(IMPORT_ERR_MSG)
-
 from .allsmt import AllSMTSolver
