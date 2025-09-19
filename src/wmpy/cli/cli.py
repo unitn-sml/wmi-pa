7e73bac441f6867d36536df17d5fee0b97c91027
paolomorettin <morettin.paolo@gmail.com>
2025-09-19 13:59:04 +0200

    typo

--- src/wmpy/cli/cli.py
+++ src/wmpy/cli/cli.py
@@ -6,24 +6,24 @@ from wmpy.cli.log import logger
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
-        description="WMI-PA CLI: A command-line interface for WMI-PA.",
+        description="WM*PY CLI: A command-line interface for WM*PY.",
         formatter_class=argparse.RawDescriptionHelpFormatter,
         epilog="Run '%(prog)s command --help' for more information on a specific command.",
     )
 
     subparsers = parser.add_subparsers(
-        dest="command", help="WMI-PA command", required=True
+        dest="command", help="WM*PY command", required=True
     )
 
     install_parser = subparsers.add_parser(
         "install",
-        help="Install dependencies for WMI-PA CLI",
+        help="Install dependencies for WM*PY CLI",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     install.add_arguments(install_parser)
     run_parser = subparsers.add_parser(
         "run",
-        help="Run WMI-PA on a given Density file",
+        help="Run WM*PY on a given Density file",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     run.add_arguments(run_parser)
